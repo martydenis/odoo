0a000daf881c7cff3602f81a2f20afdeb826560d
Martin Denis <martin.2nis@gmail.com>
2023-11-12 01:10:08 +0100

    Finished the whole thing

---
+++ resources/js/main.js
@@ -0,0 +1,9 @@
+import '../scss/theme.scss'
+
+import '../../node_modules/jquery/dist/jquery.slim.js'
+import '../../node_modules/bootstrap/js/dist/util.js'
+import '../../node_modules/bootstrap/js/dist/collapse.js'
+
+/**
+ * There was no need for actual JavaScript here, everything was done in SCSS
+ */
